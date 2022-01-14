b0ec65d5645d611f87c9671614c6721577129eab
Sam Hsu <sam.hsu@shl-group.com>
2022-01-14 10:49:17 +0800

    Add application project

---
+++ Application/Errors/RestException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace BlazorPilot.Application.Errors
+{
+    internal class RestException : Exception
+    {
+        internal HttpStatusCode Code { get; }
+
+        internal object? Errors { get; } = null;
+
+
+        public RestException(HttpStatusCode code)
+        {
+            Code = code;            
+        }
+
+        public RestException(HttpStatusCode code, object? errors = null)
+        {
+            Code = code;
+            Errors = errors;
+        }
+    }
+}
---
+++ Application/Extensions/HandlerExtensioncs.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorPilot.Application.Extensions
+{
+    public static class HandlerExtensioncs
+    {
+        public static async Task<List<T>> ToList<T>(
+            this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default)
+        {
+            var result = new List<T>();
+            await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+                result.Add(item);
+
+            return result;
+        }
+
+        public static string FullNameWithNamespace<T>(
+           this T source) where T : class
+        {
+            Type sourceType = typeof(T);
+
+            return sourceType.FullName;
+        }
+    }
+}
